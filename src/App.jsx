d826916214e3460e476ad346361c2febe05d77ca
Maite Albitre <lenna.socials@gmail.com>
2026-07-05 23:24:30 -0300

    Cambios 5/7 lena

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import React from  'react' 
+import { Route, Link, Switch } from "wouter";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import Login from './pages/Login';
+import MyProfile from './pages/MyProfile';
+import MyAppointments from './pages/MyAppointments';
+
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+        <Navbar />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/doctors" component={Doctors} />
+        <Route path="/doctors/:speciality" component={Doctors} />
+        <Route path="/login" component={Login} />
+        <Route path="/my-profile" component={MyProfile} />
+        <Route path="/my-appointments" component={MyAppointments} />
+      </Switch>
+    </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Link } from 'wouter'; 
+
+const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="navbar flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 bg-base-100 px-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow-lg border border-gray-100">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/doctors">Doctors</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </div>
+        <Link href="/" className="btn btn-ghost text-xl font-bold">Turn App</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-2 font-medium">
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/doctors">Doctors</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {isLoggedIn ? (
+          <Link href="/profile" className="btn btn-primary rounded-full px-5">Profile</Link>
+        ) : (
+          <Link href="/login" className="btn btn-outline rounded-full px-5">Log in</Link>
+        )}
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
